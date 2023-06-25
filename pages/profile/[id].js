560a8671e8c3de8ea074a75dfa17c086aafe1ef6
senadev42 <2@afrika.dev>
2023-06-25 17:25:18 +0300

    fixed load more and return home

--- pages/profile/[id].js
+++ pages/profile/[id].js
@@ -1,7 +1,12 @@
 // pages/profile/[id].js
 
+import Link from "next/link";
+
 import { useQuery, useMutation } from "@apollo/client";
 import { useRouter } from "next/router";
+
+import { useState } from "react";
+
 import fetchProfileQuery from "../../queries/fetchProfileQuery.js";
 
 import Profile from "../../components/Profile.js";
@@ -11,13 +16,18 @@ export default function ProfilePage() {
   const router = useRouter();
   const { id } = router.query;
 
+  const ITEM_LIMIT = 5;
+
+  const [loadedItemCount, setLoadedItemCount] = useState(ITEM_LIMIT);
+
   console.log("fetching profile for", id);
+
   const { loading, error, data } = useQuery(fetchProfileQuery, {
     variables: {
       request: { profileId: id },
       publicationsRequest: {
         profileId: id,
-        publicationTypes: ["POST"], // We really only want POSTs
+        publicationTypes: ["POST"], // We  only want POSTs
       },
     },
   });
@@ -43,14 +53,39 @@ export default function ProfilePage() {
     );
   }
 
-  console.log("on profile page data: ", data);
+  // console.log("on profile page data: ", data);
+
+  const itemsToRender = data.publications.items
+    .slice(0, loadedItemCount)
+    .map((post, idx) => {
+      return <Post key={idx} post={post} />;
+    });
+
   return (
-    <div className="flex flex-col p-8 items-center">
+    <div className="flex flex-col p-8 items-center ">
+      {/* back home button */}
+
+      <Link href={`/`}>
+        <div class="border-b-2 border-gray-900 | hover:bg-gray-100 | cursor-pointer | mb-4 p-3 | self-start text-left sticky top-0">
+          Return Home
+        </div>
+      </Link>
+
+      {/* Their profile */}
       <Profile profile={data.profile} displayFullProfile={true} />
 
-      {data.publications.items.map((post, idx) => {
-        return <Post key={idx} post={post} />;
-      })}
+      {/* Their publications */}
+      {itemsToRender}
+
+      {/* Render the Load More button if there are more items to load */}
+      {loadedItemCount < data.publications.items.length && (
+        <button
+          onClick={() => setLoadedItemCount(loadedItemCount + ITEM_LIMIT)}
+          class="border-t-2 hover:bg-gray-100 border-gray-900 cursor-pointer | mb-4 p-3 | "
+        >
+          Load More
+        </button>
+      )}
     </div>
   );
 }
